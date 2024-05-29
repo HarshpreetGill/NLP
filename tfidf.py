b4b2ecd219821cf7a3691d48c43ac554dcd63d8f
HarshpreetGill <harshgillmiani@gmail.com>
2024-05-29 19:58:53 +0530

    tf-idf

--- tfidf.py
+++ tfidf.py
@@ -1,41 +1,11 @@
-import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-import re
 from sklearn.feature_extraction.text import TfidfVectorizer
-
+import pandas as pd
 #sample corpus
-corpus = ['The quick brown fox jumps over the lazy dog.',
-        'The lazy dog likes to sleep all day.',
-        'The brown fox prefers to eat cheese.',
-        'The red fox jumps over the brown fox.',
-        'The brown dog chases the fox'
-        ]
+df = pd.DataFrame({'text': ['people watch campusx', 'campusx watch campusx', 'people write comment', 'campusx write comment'], 'output':[1,1,0,0]})
+tfidf = TfidfVectorizer()
+tfidf.fit_transform(df['text']).toarray()
 
-#preprocess text
+print(tfidf.idf_) #idf value is constt
+print(tfidf.get_feature_names_out())
 
-def preprocess_text(text):
-    text = re.sub('[a-zA-Z]',' ',text)
-    words =  word_tokenize(text.lower())
-    words = [word for word in words if word not in stopwords.words('english')]
-    return ' '.join(words)
-
-# preprocess corpus
-corpus = [preprocess_text(doc) for doc in corpus]
-print('corpus: \n{}'.format(corpus))
-
-# Create a TfidfVectorizer object and fit it to the preprocessed corpus
-vectorizer = TfidfVectorizer()
-vectorizer.fit(corpus)
-
-# Transform the preprocessed corpus into a TF-IDF matrix
-tf_idf_matrix = vectorizer.transform(corpus)
-
-# Get list of feature names that correspond to the columns in the TF-IDF matrix
-print("Feature Names:\n", vectorizer.get_feature_names_out())
-
-# Print the resulting matrix
-print("TF-IDF Matrix:\n",tf_idf_matrix.toarray())
     
